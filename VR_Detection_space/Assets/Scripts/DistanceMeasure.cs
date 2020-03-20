a230c29c9052780958be9e5ff3fbfe934be974bb
AlexanderSRasmussen <asra15@student.aau.dk>
2020-03-20 09:13:10 +0100

    Scene and distance script

---
+++ VR_Detection_space/Assets/Scripts/DistanceMeasure.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistanceMeasure : MonoBehaviour
+{
+
+    public GameObject object1;
+    public GameObject object2;
+    public float currentDistance;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DistanceCalculator(object1, object2);
+    }
+
+    public void DistanceCalculator(GameObject object1, GameObject object2)
+    {
+        float distance = Vector3.Distance(object1.transform.position, object2.transform.position);
+        currentDistance = distance;
+    }
+}
